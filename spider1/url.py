ccb0dfb1f32449b66cd0ef9365f24a45a1f45154
CharmaineKohJiaYIng <charmaine.koh245@gmail.com>
2022-02-27 22:18:51 +0800

    xxx

---
+++ spider1/url.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class NewSpider(scrapy.Spider):
+    name = "New_Spider"
+    start_urls = ['http://192.168.137.138/hr2/']
+
+    def parse(self, response):
+        for x in response.css('img'):
+            yield {
+                'Image Link': x.xpath('@src').extract_first(),
+            }
+            Page_selector = '.next a ::attr(herf)'
+            next_page = response.css(Page_selector).extract_first()
+            if next_page:
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parse
+                )
